568db41e8b1ebacaa7ed55fffa7cdcf6424d1250
fungerouscode <duchanhstyle@gmail.com>
2021-08-30 16:02:50 +0700

    working with enums

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -1,14 +1,20 @@
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR = 200,
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
-  role: [number, string];
+  role: Role;
 } = {
   // const person = {
   name: "Harry Nguyen",
   age: 21,
   hobbies: ["Sports", "Cooking"],
-  role: [1, "amdin"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("mod");
@@ -22,4 +28,8 @@ console.log(person.name);
 
 for (let hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is Admin");
 }
