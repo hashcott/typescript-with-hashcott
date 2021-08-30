a874e5a6e5f8f8abfaaea14f08137a93aa7eb3b1
fungerouscode <duchanhstyle@gmail.com>
2021-08-30 15:52:35 +0700

    working with tuples

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -1,12 +1,19 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  // const person = {
   name: "Harry Nguyen",
   age: 21,
   hobbies: ["Sports", "Cooking"],
+  role: [1, "amdin"],
 };
+
+// person.role.push("mod");
+
+// person.role = [1, "admin", "mod"]; Error
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
