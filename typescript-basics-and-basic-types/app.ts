4cf5eb31985c0c6fa1308fd6f9bd737acb7fdf8d
fungerouscode <duchanhstyle@gmail.com>
2021-08-30 15:47:53 +0700

    array types

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -1,10 +1,18 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: "Harry Nguyen",
   age: 21,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (let hobby of person.hobbies) {
+  console.log(hobby);
+}
