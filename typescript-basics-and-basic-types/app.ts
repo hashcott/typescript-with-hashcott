69aeb09f05aeb25f627898faa7afb5a4f7581535
fungerouscode <duchanhstyle@gmail.com>
2021-08-31 10:20:40 +0700

    Literal type

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -1,14 +1,21 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(19, 20);
+const combinedAges = combine(19, 20, "as-number");
 console.log(combinedAges);
-const combinedNames = combine("Harry", " Nguyen");
+const combinedNames = combine("Harry", " Nguyen", "as-text");
 console.log(combinedNames);
