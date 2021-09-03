1281a07e5f0b3ca234ca0a92c48881fb0b14c603
fungerouscode <duchanhstyle@gmail.com>
2021-09-03 22:10:49 +0700

    function types and callbacks

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -6,6 +6,11 @@ function printResult(n: number): void {
   console.log("Result", n);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(10, 4));
 
 let combineValues: (a: number, b: number) => number;
@@ -14,3 +19,7 @@ combineValues = add;
 // combineValues = printResult;
 
 console.log(combineValues(10, 10));
+
+addAndHandle(10, 15, (result) => {
+  console.log(result);
+});
