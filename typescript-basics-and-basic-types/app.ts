397109d5b5dbf2587b74f1b2d2178af40420381e
fungerouscode <duchanhstyle@gmail.com>
2021-09-03 22:23:37 +0700

    the never type

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -8,4 +8,9 @@ if (typeof userInput === "string") {
   userName = userInput;
 }
 
+function generateError(message: string, code: number) {
+  throw { message, code };
+}
+
 console.log(typeof userName);
+generateError("An error occurred!", 500);
