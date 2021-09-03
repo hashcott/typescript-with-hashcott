345c6ee39d62109143db2cfd807d0da5ee4c479a
fungerouscode <duchanhstyle@gmail.com>
2021-09-03 22:02:45 +0700

    funtion return types & void

--- typescript-basics-and-basic-types/app.ts
+++ typescript-basics-and-basic-types/app.ts
@@ -1,24 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(19, 20, "as-number");
-console.log(combinedAges);
-const combinedNames = combine("Harry", " Nguyen", "as-text");
-console.log(combinedNames);
+function printResult(n: number): void {
+  console.log("Result", n);
+}
+
+printResult(add(10, 4));
