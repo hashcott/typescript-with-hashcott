6939beeb6d8913b2f605ce539de6395f7da2be9e
fungerouscode <duchanhstyle@gmail.com>
2021-09-04 23:19:52 +0700

    using interface with classese

--- 04-classes-interface/src/classes.ts
+++ 04-classes-interface/src/classes.ts
@@ -1,16 +1,22 @@
-interface Person {
+interface Greetable {
   name: string;
   age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 21;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: "Harry",
-  age: 21,
   greet(phrase: string) {
-    console.log(phrase);
-  },
-};
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user1 = new Person("Harry");
+
+user1.greet("Hello");
