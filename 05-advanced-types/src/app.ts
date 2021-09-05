f8f2b95bf2fed535b58480bc71b95551e86aa721
fungerouscode <duchanhstyle@gmail.com>
2021-09-05 22:00:12 +0700

    intersection types

---
+++ 05-advanced-types/src/app.ts
@@ -0,0 +1,18 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Harry",
+  privileges: ["Admin"],
+  startDate: new Date(),
+};
