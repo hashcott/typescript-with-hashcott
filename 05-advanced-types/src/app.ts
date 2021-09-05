9fab682900ca24cf1a7e43415f19ad540001a100
fungerouscode <duchanhstyle@gmail.com>
2021-09-05 22:47:29 +0700

    indexes properties

--- 05-advanced-types/src/app.ts
+++ 05-advanced-types/src/app.ts
@@ -106,3 +106,12 @@ const userInput = document.getElementById("user-input");
 if (userInput) {
   (userInput as HTMLInputElement).value = "Hi there!";
 }
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email",
+  username: "Must start with a capital character",
+};
