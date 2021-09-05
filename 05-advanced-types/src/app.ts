4250319bfcde598b782c9084e9898b9fdf6f000f
fungerouscode <duchanhstyle@gmail.com>
2021-09-05 22:51:52 +0700

    function overloads

--- 05-advanced-types/src/app.ts
+++ 05-advanced-types/src/app.ts
@@ -22,12 +22,20 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // never when not match
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+
 function add(a: Combinable, b: Combinable) {
   if (typeof a == "string" || typeof b == "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const naming = add("Harry", "Potter");
+naming.split(" ");
 
 type UnknownEmployee = Employee | Admin;
 
