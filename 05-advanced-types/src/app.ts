e1588285159415d4948638665e9bfbfe0baeedd6
fungerouscode <duchanhstyle@gmail.com>
2021-09-05 22:31:20 +0700

    discriminated unions

--- 05-advanced-types/src/app.ts
+++ 05-advanced-types/src/app.ts
@@ -72,3 +72,30 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed:", speed);
+}
+
+moveAnimal({ type: "horse", runningSpeed: 100 });
