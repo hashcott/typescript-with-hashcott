6fc8b11788ef17c6b4d102585681eee7a14291b5
fungerouscode <duchanhstyle@gmail.com>
2021-09-06 21:28:23 +0700

    built-in generics

---
+++ 06-generics/src/app.ts
@@ -0,0 +1,12 @@
+const names: Array<string> = []; // string[]
+// names[0].split(" ");
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  data.split(" ");
+});
