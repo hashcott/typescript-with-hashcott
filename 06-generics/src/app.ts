7617d36ddd2b5b9554f3102709ea4a11daa7e772
fungerouscode <duchanhstyle@gmail.com>
2021-09-06 21:57:14 +0700

    working with constraints

--- 06-generics/src/app.ts
+++ 06-generics/src/app.ts
@@ -11,7 +11,7 @@
 //   data.split(" ");
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
