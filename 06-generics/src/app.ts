1d9db18a800628ae70a022634cb2cd83e7ddb163
fungerouscode <duchanhstyle@gmail.com>
2021-09-06 21:50:19 +0700

    creating a generic function

--- 06-generics/src/app.ts
+++ 06-generics/src/app.ts
@@ -1,12 +1,23 @@
-const names: Array<string> = []; // string[]
-// names[0].split(" ");
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge<{ name: string }, { age: number }>(
+  { name: "Max" },
+  { age: 10 }
+);
+
+console.log(mergeObj.name);
