96ae1b75ebfb12e6f54b077fdbb80863034004bc
fungerouscode <duchanhstyle@gmail.com>
2021-09-06 22:32:35 +0700

    generic classes

--- 06-generics/src/app.ts
+++ 06-generics/src/app.ts
@@ -45,3 +45,25 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem("Harry");
+textStorage.addItem("Potter");
+
+console.log(textStorage.getItems());
