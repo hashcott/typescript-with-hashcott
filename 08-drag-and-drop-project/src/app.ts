730e649d44b75c4ed4a5795d6943ff6eb57fb423
insecures <duchanhstyle@gmail.com>
2021-09-13 23:46:01 +0700

    create a re-usable validate function

--- 08-drag-and-drop-project/src/app.ts
+++ 08-drag-and-drop-project/src/app.ts
@@ -1,12 +1,41 @@
-// autobind decorator
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
 
+function validate(validateInput: Validatable) {
+  let isValid = true;
+  if (validateInput.required) {
+    isValid = isValid && validateInput.value.toString().trim().length !== 0;
+  }
+  if (validateInput.minLength && typeof validateInput.value === "string") {
+    isValid = isValid && validateInput.value.length >= validateInput.minLength;
+  }
+  if (validateInput.maxLength && typeof validateInput.value === "string") {
+    isValid = isValid && validateInput.value.length <= validateInput.maxLength;
+  }
+  if (validateInput.min && typeof validateInput.value === "number") {
+    isValid = isValid && validateInput.value >= validateInput.min;
+  }
+
+  if (validateInput.max && typeof validateInput.value === "number") {
+    isValid = isValid && validateInput.value <= validateInput.max;
+  }
+  console.log(isValid);
+
+  return isValid;
+}
+
+// autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     get() {
-      console.log(this);
-
       const boundFn = originalMethod.bind(this);
       return boundFn;
     },
@@ -43,10 +72,51 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+    if (
+      !validate(titleValidatable) &&
+      !validate(descriptionValidatable) &&
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again");
+      return;
+    }
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @autobind
   private submitHandle(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      // const [title, description, people] = userInput;
+
+      this.clearInput();
+    }
   }
 
   private configure() {
