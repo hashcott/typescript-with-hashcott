e025c30c9ca043ad6fd32e7f0fba60b3aaf43c76
insecures <duchanhstyle@gmail.com>
2021-09-13 23:58:18 +0700

    rendering project list

--- 08-drag-and-drop-project/src/app.ts
+++ 08-drag-and-drop-project/src/app.ts
@@ -128,4 +128,33 @@ class ProjectInput {
   }
 }
 
+// PROJECT LIST
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importNode = document.importNode(this.templateElement.content, true);
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    this.element.id = `${type}-projects`;
+    this.renderContent();
+    this.attach();
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
