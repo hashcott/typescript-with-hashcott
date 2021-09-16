27a6fd478fcfafdce2dc0ec7c9d578a1ec9edafb
insecures <duchanhstyle@gmail.com>
2021-09-16 16:30:31 +0700

    organizing files and folders

---
+++ 08-drag-and-drop-project/src/components/project-item.ts
@@ -0,0 +1,50 @@
+/// <reference path="./base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+
+namespace App {
+  // ProjectItem
+
+  export class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable
+  {
+    private project: Project;
+    get persons(): string {
+      if (this.project.people === 1) {
+        return "1 person";
+      } else if (this.project.people > 1) {
+        return this.project.people + " persons";
+      }
+      return "";
+    }
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id);
+      this.project = project;
+      this.configure();
+      this.renderContent();
+    }
+    @autobind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData("text/plain", this.project.id);
+      event.dataTransfer!.effectAllowed = "move";
+    }
+
+    @autobind
+    dragEndHandler(_: DragEvent) {
+      console.log("Drag End");
+    }
+
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler);
+      this.element.addEventListener("dragend", this.dragEndHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector("h3")!.textContent = this.persons;
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+}
---
+++ 08-drag-and-drop-project/src/util/validation.ts
@@ -0,0 +1,34 @@
+namespace App {
+  // Validation
+  export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  export function validate(validateInput: Validatable) {
+    let isValid = true;
+    if (validateInput.required) {
+      isValid = isValid && validateInput.value.toString().trim().length !== 0;
+    }
+    if (validateInput.minLength && typeof validateInput.value === "string") {
+      isValid =
+        isValid && validateInput.value.length >= validateInput.minLength;
+    }
+    if (validateInput.maxLength && typeof validateInput.value === "string") {
+      isValid =
+        isValid && validateInput.value.length <= validateInput.maxLength;
+    }
+    if (validateInput.min && typeof validateInput.value === "number") {
+      isValid = isValid && validateInput.value >= validateInput.min;
+    }
+
+    if (validateInput.max && typeof validateInput.value === "number") {
+      isValid = isValid && validateInput.value <= validateInput.max;
+    }
+    return isValid;
+  }
+}
